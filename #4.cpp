8fc828811dc42399032d6d5bdcf2d188ed735a7e
Kirikaito <116945391+Kirikaito@users.noreply.github.com>
2022-12-04 18:19:37 +0500

    Create #4.cpp

---
+++ #4.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    float a,b;
+    int n = 1,m,ch = 0;
+    string c;
+    cin >> a >> b;
+    c = to_string(a/b);
+    for(int j = 1; j <= (c.size()/2) - 1;j++){   
+        for(int i = j + 2; i < c.size() - 1;i++){
+            if(c[i] == c[i - j]){
+                n += 1;
+            }else{
+                n = 1;
+            }
+        }
+        if(n != 1){
+            m = j;
+            ch = 1;
+            break;
+        } 
+    }
+    while(n > 0){
+        c.erase(c.end() - n);
+        n--;
+    }
+    if(ch == 1){
+        c = c +')';
+        c.insert(c.end() - (m + 1),'(');
+    }
+    cout << c ;
+}
